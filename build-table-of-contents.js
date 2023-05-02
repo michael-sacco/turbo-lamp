87cb3e8129e4319760c1905ebb85856caa94efbe
Michael Sacco <michael@occasoftware.com>
2023-05-02 15:48:59 -0400

    Corrected scripts

--- build-table-of-contents.js
+++ build-table-of-contents.js
@@ -9,6 +9,38 @@ function generateIdBasedOnTitle(titletoconvert) {
     .replace(/-+$/g,"");
 }
 
+
+function CopyURLToClipboard(e) {
+    navigator.clipboard.writeText(e.href);
+}
+
+function addElement(currentDiv, id) {
+    const newDiv = document.createElement("a");
+    newDiv.href = "#" + id;
+    newDiv.className = "dynamic-anchor-link"
+    newDiv.addEventListener("click", function () { navigator.clipboard.writeText(this.href); });
+    const newContent = document.createTextNode("#");
+    newDiv.appendChild(newContent);
+    currentDiv.appendChild(newDiv);
+}
+
+const observer = new IntersectionObserver(entries => {
+    entries.every(entry => {
+        const id = entry.target.getAttribute("id");
+        if (entry.isIntersecting) {
+            document.querySelectorAll(".active").forEach((z) => {
+                z.classList.remove("active")
+            });
+            const activeElement = document.querySelector(`a[href="#${id}"]`);
+            activeElement.classList.add("active")
+            activeElement.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "nearest" });
+            return false;
+        }
+        return true;
+    });
+}, { rootMargin: '0% 0px -90% 0px', threshold: 0.01 });
+
+
 async function BuildTOC()
 {
     document.getElementById("content").querySelectorAll("#content > div").forEach(function (section, i)
